4979a1240b327b7c86abf0fa629d5d2bfa925878
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-04-15 17:52:42 +1000

    Set up Add to Favourite component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavourites from "./components/AddFavourites";
 
 function App() {
 
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=71047963`;
@@ -25,6 +27,11 @@ function App() {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const AddFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  }
+
   return (
     <div className="container-fluid mymov">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -35,9 +42,18 @@ function App() {
       <div className="row">
         <MovieList 
           movies={movies}
+          handleFavouritesClick={AddFavouriteMovie}
+          favouriteComponent={AddFavourites}
         />
       </div>
-      
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className='row'>
+        <MovieList 
+          movies={favourites}
+        />
+      </div>
     </div>
   );
 }
